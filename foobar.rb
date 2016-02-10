0b2eac5f0569b01c8692686f068228c58d3259c2
Elton Lou <elou@berkeley.edu>
2016-02-10 14:41:39 -0800

    1st hw

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,32 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a) #takes in an array of integers as strings, 
+    #changes each string to an integer, adds two to each number, 
+    #keeps the even numbers only, takes out any duplicates, 
+    #reject the resulting numbers greater than 10, and 
+    #returns the sum of the resulting array
+    require 'set'
+    for i in 0..a.length - 1 # converts each string to integer and adds two to each number
+        temp = a[i].to_i + 2
+        a[i] = temp
+    end
+    arr = Array.new
+    for j in 0..a.length - 1 # keeps the even numbers only
+        if a[j] % 2 == 0
+            arr = arr << a[j]
+        end
+    end
+    arr = arr.uniq
+    for k in 0..arr.length - 1
+        temp = arr[k]
+        if temp > 10
+            arr.delete(temp)
+        end
+    end
+    sum = 0
+    for l in 0..arr.length - 1
+        sum = sum + arr[l]
+    end
+    sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,27 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  temp = (a + b)
+  temp * temp
+
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  temp = a.sort
+  for i in 0..a.length - 1
+    temp[i] = temp[i] + 1
+  end
+  temp
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  temp = "#{first_name}" + " " + "#{last_name}"
+  temp
 end
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
+  require './foobar' # loads the ./foobar file so you can access the classes and methods inside
   Foobar.baz a
 end
